f85569fd80c6b95ee2e16fb7944731a023c83a09
Toonieofficial <info.toonie@gmail.com>
2024-11-04 20:49:37 +0330

    change menu structure and add sub-menu

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,8 +1,10 @@
 // components/Header.tsx
-
+"use client";
+import { useState } from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
 export default function Header() {
+const [hovered, setHovered] = useState(false); 
   return (
     <header className="sticky flex justify-between items-center mt-8 top-10 z-50">
       <div className="container bg-black flex justify size-auto items-center rounded-full ml-auto text-white py-5 px-6" style={{border:'1.5px solid white' , }}>
@@ -13,25 +15,48 @@ export default function Header() {
         </Link>
         
         {/* Navigation Links */}
-        <nav className="w-full hidden md:flex space-x-[45px] ml-8 text-white font-medium">
-          <Link href="/about" className="text-white text-[18px] hover:text-toonie transition duration-200">
-            About Us
-          </Link>
-          <Link href="/wealth/financial-managment" className="text-white text-[18px] hover:text-toonie transition duration-200">
-            Wealth
-          </Link>
-          <Link href="/career" className="text-white text-[18px] hover:text-toonie transition duration-200">
-            Career
-          </Link>
-          <Link href="/wealth/insurance" className="text-white text-[18px] hover:text-toonie transition duration-200">
-            Services
-          </Link>
-          <Link href="/wealth/tax" className="text-white text-[18px] hover:text-toonie transition duration-200">
-            Contact
-          </Link>
-          <Link href="/wealth/invesment" className="text-white text-[18px] hover:text-toonie transition duration-200">
-            Get Toonie
-          </Link>
+        <nav className="w-full hidden md:flex space-x-[55px] ml-10 text-white font-medium">
+        <ul className="flex space-x-10">
+        <li 
+          className="relative group"
+          onMouseEnter={() => setHovered(true)}
+          onMouseLeave={() => setHovered(false)}
+        >
+          <button className="hover:text-toonie">
+            WEALTH
+          </button>
+
+          {/* Submenu */}
+          {hovered && (
+            <ul 
+              className="absolute top-4 left-0 mt-1 w-[300px] bg-gray-800 text-white rounded-lg shadow-lg py-5"
+              onMouseEnter={() => setHovered(true)}
+              onMouseLeave={() => setHovered(false)}
+            >
+              <li className="px-4 py-2 hover:bg-toonie">
+                <Link href="/wealth/financial-management">Financial Management</Link>
+              </li>
+              <li className="px-4 py-2 hover:bg-toonie">
+                <Link href="/wealth/insurance">Insurance</Link>
+              </li>
+              <li className="px-4 py-2 hover:bg-toonie">
+                <Link href="/wealth/invesment">Investment</Link>
+              </li>
+              <li className="px-4 py-2 hover:bg-toonie">
+                <Link href="/wealth/tax">Tax</Link>
+              </li>
+              <li className="px-4 py-2 hover:bg-toonie">
+                <Link href="/corporate">Corporate</Link>
+              </li>
+            </ul>
+          )}
+        </li>
+        {/* Other Menu Items */}
+        <li><Link href="/money" className="hover:text-toonie">MONEY</Link></li>
+        <li><Link href="/get-toonie" className="hover:text-toonie">GET TOONIE</Link></li>
+        <li><Link href="/career" className="hover:text-toonie">CAREER</Link></li>
+        <li><Link href="/about" className="hover:text-toonie">ABOUT</Link></li>
+      </ul>
         </nav>
         
       </div>
